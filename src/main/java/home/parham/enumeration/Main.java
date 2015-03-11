5eff9e01d5ae2214847e32d07b8da68bd302f178
Parham Alvani <parham.alvani@gmail.com>
2015-03-11 10:25:53 +0330

    enumeration class improved.

--- src/main/java/home/parham/enumeration/Main.java
+++ src/main/java/home/parham/enumeration/Main.java
@@ -13,10 +13,19 @@
 
 package home.parham.enumeration;
 
+import java.util.EnumSet;
+
 public class Main {
 	public static void main(String args[]){
 		System.out.println(PEnum.A.getValue());
 		System.out.println(PEnum.A.name());
 		System.out.println(PEnum.B.getValue());
+
+		/* print all values in enum PNum */
+		for (PEnum element : PEnum.values())
+			System.out.println(element.getValue());
+
+		for (PEnum element : EnumSet.range(PEnum.A, PEnum.B))
+			System.out.println(element.getValue());
 	}
 }
