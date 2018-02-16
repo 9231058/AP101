44928d4d7f85af94f479842c8a4a517a774d7620
Parham Alvani <parham.alvani@gmail.com>
2018-02-16 22:49:27 +0330

    Adds argument example.

---
+++ src/main/java/com/github/autceit/ap101/s01/e04/Args.java
@@ -0,0 +1,9 @@
+package com.github.autceit.ap101.s01.e04;
+
+public class Args {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
