6f1d5b4846a982fda45ed1c0b6a911f16457c289
Parham Alvani <parham.alvani@gmail.com>
2018-02-16 21:25:58 +0330

    Corrects java doc.

--- src/main/java/com/github/autceit/ap101/s01/e01/HelloWorld.java
+++ src/main/java/com/github/autceit/ap101/s01/e01/HelloWorld.java
@@ -7,15 +7,15 @@
  *
  * [] Created By : Parham Alvani <parham.alvani@gmail.com>
  * =======================================
-*/
-/**
- * @author Parham Alvani
-*/
+ */
 
 package com.github.autceit.ap101.s01.e01;
 
+/**
+ * @author Parham Alvani
+ */
 public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello world");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
