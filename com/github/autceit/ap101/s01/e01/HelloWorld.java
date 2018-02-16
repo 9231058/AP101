3ead9fe1885046da6f7dbcd686b2c172adf760fc
Parham Alvani <parham.alvani@gmail.com>
2018-02-16 21:02:14 +0330

    Adds hello world example.

---
+++ com/github/autceit/ap101/s01/e01/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : com/github/aut-ceit/ap101/s01/e01/HelloWorld.java
+ *
+ * [] Creation Date : 16-02-2018
+ *
+ * [] Created By : Parham Alvani <parham.alvani@gmail.com>
+ * =======================================
+*/
+/**
+ * @author Parham Alvani
+*/
+
+package com.github.autceit.ap101.s01.e01;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
