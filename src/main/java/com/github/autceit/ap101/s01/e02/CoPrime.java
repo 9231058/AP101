7b5baa5ac8aa949406bc7ef5d069a8a951228d07
Parham Alvani <parham.alvani@gmail.com>
2018-02-16 21:44:05 +0330

    Adds Co-Prime example.

---
+++ src/main/java/com/github/autceit/ap101/s01/e02/CoPrime.java
@@ -0,0 +1,25 @@
+package com.github.autceit.ap101.s01.e02;
+
+import java.util.Scanner;
+
+public class CoPrime {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        int A = a;
+        int B = b;
+
+        while (a != 0) {
+            int t = a;
+            a = b % a;
+            b = t;
+        }
+        if (b == 1) {
+            System.out.printf("%d and %d are co-prime integers", A, B);
+        } else {
+            System.out.printf("%d and %d aren't co-prime integers and have gcd = %d", A, B, b);
+        }
+    }
+}
